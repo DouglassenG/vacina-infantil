beb0b8da69a5e323325847eef50f76da8f762b86
Douglas <douglasmsrodrigues@hotmail.com>
2026-06-20 19:21:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.desafio.vacina.infantil',
+  appName: 'vacina-infantil',
+  webDir: 'www'
+};
+
+export default config;
