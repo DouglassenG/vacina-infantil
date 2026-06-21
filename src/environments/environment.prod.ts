01684bd232375a01d6b0d20cf3838b929d1c2be3
Douglas <douglasmsrodrigues@hotmail.com>
2026-06-21 17:26:59 -0300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDZSTfq_ttq9EeMDtIRABh6zx1BSLCazdQ",
+    authDomain: "vacina-infantil-c0344.firebaseapp.com",
+    projectId: "vacina-infantil-c0344",
+    storageBucket: "vacina-infantil-c0344.firebasestorage.app",
+    messagingSenderId: "944647196385",
+    appId: "1:944647196385:web:02a8ba3d5849be15b071a0"
+  }
 };
